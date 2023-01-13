2bd4e5629c77e247b929e98e5c27253875f49529
lokywolf2295 <matiasesek1995@gmail.com>
2023-01-13 10:02:19 -0300

    I add methods depositar, retirar and transferir

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,3 +1,4 @@
+
 public class Cuenta {
 
     double saldo;
@@ -5,4 +6,26 @@ public class Cuenta {
     int numero;
     String titular;
 
+    //no retorna un valor
+    public void depositar(double valor) {
+        this.saldo = this.saldo + valor;
+    }
+
+    //retorna un valor booleano
+    public boolean retirar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transferir(double valor, Cuenta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
 }
