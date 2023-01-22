1ddaf6c999ba3cccd90495560870e7fb6c4e1c88
lokywolf2295 <matiasesek1995@gmail.com>
2023-01-22 10:02:06 -0300

    I create the TestString class

---
+++ src/polimorfismo/test/TestString.java
@@ -0,0 +1,44 @@
+package polimorfismo.test;
+
+import polimorfismo.modelo.CuentaAhorros;
+
+public class TestString {
+
+    public static void main(String[] args) {
+
+        //primera forma de crear un string mas usada
+        String nombre = "Alura"; // Inmutable
+
+        // segunda forma de crear un string No utilizada en el mundo real
+         String nombre2 = new String("Alura");
+        System.out.println("Antes de metodo: " + nombre);
+        nombre = nombre.replace("A", "a");
+        nombre = nombre.concat(" cursos online");
+        nombre = nombre.toUpperCase();
+        nombre = nombre.toLowerCase();
+        //01234
+        char letra = nombre.charAt(3);
+        int indice = nombre.indexOf("e");
+
+        Object cuenta = new CuentaAhorros(344, 544);
+
+        printLine(nombre);
+        printLine(letra);
+        printLine(indice);
+        printLine(new CuentaAhorros(200, 300));
+
+    }
+
+    public static void printLine(Object valor) {
+        System.out.println(valor.toString());
+    }
+//	public static void printLine(String valor) {
+//		System.out.println(valor);
+//	}
+//	public static void printLine(char valor) {
+//		System.out.println(valor);
+//	}
+//	public static void printLine(CuentaAhorros valor) {
+//		System.out.println(valor);
+//	}
+}
