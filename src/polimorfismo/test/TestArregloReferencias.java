8fea16e2dfdff8721d6da283ae8fff84b82e0ad3
lokywolf2295 <matiasesek1995@gmail.com>
2023-01-23 09:57:29 -0300

    I create 2 classes for array practice

---
+++ src/polimorfismo/test/TestArregloReferencias.java
@@ -0,0 +1,28 @@
+package polimorfismo.test;
+
+import polimorfismo.modelo.CuentaCorriente;
+
+public class TestArregloReferencias {
+    
+    public static void main(String[] args) {
+		
+		CuentaCorriente cc = new CuentaCorriente(23, 44);
+		
+		
+		//                          [ new |cc |null  |null  |null  ]
+		CuentaCorriente[] cuentas = new CuentaCorriente[5];
+		cuentas[1] = cc;
+		
+		System.out.println(cc);
+		System.out.println(cuentas[1]);
+		
+		cuentas[0] = new CuentaCorriente(11, 99);
+		System.out.println(cuentas[0]);
+		
+		for (int i = 0; i < cuentas.length; i++) {
+			System.out.println(cuentas[i]);
+		}
+		
+		
+	}
+}
