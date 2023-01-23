7de439d142027dc29124c130a60c4c7c6e1107dd
lokywolf2295 <matiasesek1995@gmail.com>
2023-01-23 20:08:00 -0300

    I practice about references

--- src/polimorfismo/test/TestArregloReferencias.java
+++ src/polimorfismo/test/TestArregloReferencias.java
@@ -1,28 +1,37 @@
 package polimorfismo.test;
 
-import polimorfismo.modelo.CuentaCorriente;
+import polimorfismo.modelo.*;
 
 public class TestArregloReferencias {
-    
+
     public static void main(String[] args) {
-		
-		CuentaCorriente cc = new CuentaCorriente(23, 44);
-		
-		
-		//                          [ new |cc |null  |null  |null  ]
-		CuentaCorriente[] cuentas = new CuentaCorriente[5];
-		cuentas[1] = cc;
-		
-		System.out.println(cc);
-		System.out.println(cuentas[1]);
-		
-		cuentas[0] = new CuentaCorriente(11, 99);
-		System.out.println(cuentas[0]);
-		
-		for (int i = 0; i < cuentas.length; i++) {
-			System.out.println(cuentas[i]);
-		}
-		
-		
-	}
+
+        //                          [ new |cc |null  |null  |null  ]
+        Object[] referencias = new Object[5];
+
+        CuentaCorriente cc = new CuentaCorriente(23, 44);
+
+        referencias[1] = cc;
+
+        Cliente cliente = new Cliente();
+        referencias[4] = cliente;
+
+        Cliente obtenido = (Cliente) referencias[4];
+        System.out.println(obtenido);
+
+        CuentaAhorros ca = new CuentaAhorros(44, 55);
+        referencias[3] = ca;
+
+        referencias[0] = new CuentaCorriente(11, 99);
+        System.out.println(referencias[0]);
+
+        // Cast
+        CuentaCorriente cuenta = (CuentaCorriente) referencias[1];
+        System.out.println(cuenta);
+
+        for (int i = 0; i < referencias.length; i++) {
+            System.out.println(referencias[i]);
+        }
+
+    }
 }
